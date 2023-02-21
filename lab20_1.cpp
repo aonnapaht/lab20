e02e5b1ab90340ea6ffa458f16f42e6ed8be953d
aonnapaht <120167959+aonnapaht@users.noreply.github.com>
2023-02-21 17:29:09 +0700

    Update lab20_1.cpp

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect A,Rect B){
+   double x = min(A.w+A.x,B.w+B.x) - max(A.x,B.x);
+   double y = min(A.y,B.y) - max(A.y-A.h,B.y-B.h);
+   if(x*y < 0) return 0;
+   return x*y;   
 }
